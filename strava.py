0636e795bdd6da3b0c9a92b818557195fdf99f24
aaronpalmer <palmerac@rogers.com>
2024-05-05 15:01:51 -0400

    st-2 0505

--- strava.py
+++ strava.py
@@ -2,13 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import streamlit as st
+import numpy as np
 from datetime import timedelta
 import seaborn as sns
 
 plt.style.use("ggplot")
 st.set_page_config(layout="wide")
 
-df = pd.read_csv("HeartWatch-Workouts-20230717-to-20231106.csv")
+st.sidebar.markdown("## Upload your own data")
+st.sidebar.markdown('Export standard Workouts via Heartwatch app')
+uploaded_file = st.sidebar.file_uploader("Upload your file", type=["csv"])
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+else:
+    df = pd.read_csv("HeartWatch-Workouts-20230717-to-20231106.csv")
 
 df = df.drop(
     [
@@ -81,13 +88,20 @@ for g in [run, bike, walk, golf]:
 # Streamlit main page
 st.title("Workout Dashboard")
 
-st.write(f"Total Activies: {len(df)}")
-st.write(f"Total Duration: {convert_to_time_string(df.sDuration.sum())}")
-st.write(f"Average Duration: {convert_to_time_string(df.sDuration.sum()/len(df))}")
-st.write(f"Total Distance: {df.km.sum()} km")
-st.write(f"Average Distance: {round(df.km.mean(),2)} km")
-st.write(f"Total Heart Beats: {df.totalBeats.sum():,.0f}")
-st.write(f"Average BPM: {avghr(df)}")
+col1, col2 = st.columns(2)
+with col1:
+    st.write(f"Total Activies: {len(df)}")
+    st.write(f"Total Duration: {convert_to_time_string(df.sDuration.sum())}")
+    st.write(f"Total Distance: {df.km.sum()} km")
+    st.write(f"Total Heart Beats: {df.totalBeats.sum():,.0f}")
+    st.write(f"Total Calories Burned: {df.Cals.sum():,.0f}")
+with col2:
+    st.markdown('\n')
+    st.write(f"Average Duration: {convert_to_time_string(df.sDuration.sum()/len(df))}")
+    st.write(f"Average Distance: {round(df.km.mean(),2)} km")
+    st.write(f"Average BPM: {avghr(df)}")
+    st.write(f"Average Calories Burned: {round(df.Cals.sum() / len(df),2)}")
+    st.write(f"Average Calories/Hour: {round(df.Cals.sum() / df.hDuration.sum(),2)}")
 
 # Activity pages
 def display_summary_and_raw_data(data, title):
@@ -99,18 +113,23 @@ def display_summary_and_raw_data(data, title):
             st.write(f"Total {title}: {len(data)}")
             st.write(f"Total Duration: {convert_to_time_string(data.sDuration.sum())}")
             st.write(f"Total Distance: {data.km.sum()} km")
-            st.write(f"Total Heart Beats: {data.totalBeats.sum():,.0f}")    
+            st.write(f"Total Heart Beats: {data.totalBeats.sum():,.0f}")  
+            st.write(f"Total Calories Burned: {data.Cals.sum():,.0f}")  
 
         with col2:
+            st.markdown("#")
             st.write(
                 f"Average Duration: {convert_to_time_string(data.sDuration.sum()/len(data))}"
             )
             st.write(f"Average Distance: {round(data.km.mean(),2)} km")
             st.write(f"Average BPM: {avghr(data)}")
+            st.write(f"Average Calories Burned: {round(data.Cals.mean(),2)}")
         with col3:
+            st.markdown("#")
             st.write(f"Median Duration {convert_to_time_string(data.sDuration.median())}")
             st.write(f"Median Distance: {round(data.km.median(),2)} km")
             st.write(f"Median BPM: {data['bpm-Avg.'].median()}")
+            st.write(f"Median Calories Burned: {data.Cals.median():.0f}")
     with tab2:
         with st.expander("Boxplot"):
             columns = ['km', 'mDuration', 'bpm-Avg.', 'bpm-hi', 'km/h', 'Cals/h']
@@ -119,7 +138,7 @@ def display_summary_and_raw_data(data, title):
             for i, column in enumerate(columns):
                 sns.boxplot(x=data[column], ax=axes[i])
                 axes[i].set_title(column)
-            plt.suptitle('Boxplots')
+            plt.suptitle('Boxplots', fontsize=20)
             plt.tight_layout()
             st.pyplot(fig)
         
@@ -155,6 +174,7 @@ def display_summary_and_raw_data(data, title):
             plt.title('Cumulative Distance')
             plt.ylabel('Distance (km)')
             plt.xticks(rotation=45)
+            plt.tight_layout()
             st.pyplot(fig)
 
         with st.expander('Cumulative Duration'):
@@ -163,8 +183,65 @@ def display_summary_and_raw_data(data, title):
             plt.title('Cumulative Duration')
             plt.ylabel('Time (h)')
             plt.xticks(rotation=45)
+            plt.tight_layout()
             st.pyplot(fig)
 
+        with st.expander('Distance over Time'):
+            fig, ax = plt.subplots(figsize=(10,6))
+            ax.scatter(data['cumkm'], data['km'], label='Distance', color='red')
+            m, b = np.polyfit(data['cumkm'], data['km'], 1)
+            ax.plot(data['cumkm'], m*data['cumkm'] + b, color='blue', label='Distance LOBF')
+            ax.plot(data['cumkm'], data['km'].rolling(window=5).mean(), label=f'5 {title} distance MA', color='green', linestyle='--')
+            ax.plot(data['cumkm'], data['km'].rolling(window=10).mean(), label=f'10 {title} speed MA', color='purple', linestyle='--')
+            
+            plt.title('Distance over Time')
+            plt.legend()
+            plt.xticks(rotation=45)
+            plt.xlabel('Cumulative KM')
+            plt.tight_layout()
+            st.pyplot(fig)
+
+        with st.expander("Speed over Time"):
+            fig, ax = plt.subplots(figsize=(10,6))
+            ax.scatter(data['cumkm'], data['km/h'], label='Speed(km/h)', color='red')
+            m, b = np.polyfit(data['cumkm'], data['km/h'], 1)
+            ax.plot(data['cumkm'], m*data['cumkm'] + b, color='blue', label='Speed LOBF')
+            ax.plot(data['cumkm'], data['km/h'].rolling(window=5).mean(), label=f'5 {title} speed MA', color='green', linestyle='--')
+            ax.plot(data['cumkm'], data['km/h'].rolling(window=10).mean(), label=f'10 {title} speed MA', color='purple', linestyle='--')
+            plt.title('Speed over Time')
+            plt.legend()
+            plt.xticks(rotation=45)
+            plt.xlabel('Cumulative KM')
+            plt.tight_layout()
+            st.pyplot(fig)
+
+        with st.expander("Average BPM over Time"):
+            fig, ax = plt.subplots(figsize=(10,6))
+            ax.scatter(data['cumkm'], data['bpm-Avg.'], label='HR (BPM)', color='red')
+            m, b = np.polyfit(data['cumkm'], data['bpm-Avg.'], 1)
+            ax.plot(data['cumkm'], m*data['cumkm'] + b, color='blue', label='BPM LOBF')
+            ax.plot(data['cumkm'], data['bpm-Avg.'].rolling(window=5).mean(), label=f'5 {title} BPM MA', color='green', linestyle='--')
+            ax.plot(data['cumkm'], data['bpm-Avg.'].rolling(window=10).mean(), label=f'10 {title} BPM MA', color='purple', linestyle='--')
+            plt.title('Average BPM over Time')
+            plt.legend()
+            plt.xticks(rotation=45)
+            plt.xlabel('Cumulative KM')
+            plt.tight_layout()
+            st.pyplot(fig)
+
+        with st.expander("Duration over Time"):
+            fig, ax = plt.subplots(figsize=(10,6))
+            ax.scatter(data['cumkm'], data['mDuration'], label='Duration (min)', color='red')
+            m, b = np.polyfit(data['cumkm'], data['mDuration'], 1)
+            ax.plot(data['cumkm'], m*data['cumkm'] + b, color='blue', label='Duration LOBF')
+            ax.plot(data['cumkm'], data['mDuration'].rolling(window=5).mean(), label=f'5 {title} Duration MA', color='green', linestyle='--')
+            ax.plot(data['cumkm'], data['mDuration'].rolling(window=10).mean(), label=f'10 {title} Duration MA', color='purple', linestyle='--')
+            plt.title('Duration over Time')
+            plt.legend()
+            plt.xticks(rotation=45)
+            plt.xlabel('Cumulative KM')
+            plt.tight_layout()
+            st.pyplot(fig)
     with tab4:
         st.dataframe(data)
 
