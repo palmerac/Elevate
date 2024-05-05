1f0eedae342c928d707394dc05f29b603319f2d2
aaronpalmer <palmerac@rogers.com>
2024-05-05 13:44:08 -0400

    rewrote streamlit

---
+++ strava.py
@@ -0,0 +1,196 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import streamlit as st
+from datetime import timedelta
+import seaborn as sns
+
+plt.style.use("ggplot")
+st.set_page_config(layout="wide")
+
+df = pd.read_csv("HeartWatch-Workouts-20230717-to-20231106.csv")
+
+df = df.drop(
+    [
+        "Date",
+        "from",
+        "to",
+        "rpe",
+        "bpm-%",
+        "bpm-90%+-%",
+        "90%+-mins",
+        "bpm-80-90%-%",
+        "80-90%-mins",
+        "bpm-70-80%-%",
+        "70-80%-mins",
+        "bpm-60-70%-%",
+        "60-70%-mins",
+        "bpm-50-60%-%",
+        "50-60%-mins",
+    ],
+    axis=1,
+)
+
+
+df["ISO"] = pd.to_datetime(df["ISO"])
+df["Duration"] = df["Duration"].astype(str)
+
+
+def convert_to_seconds(time_string):
+    hours, minutes, seconds = map(int, time_string.split(":"))
+    return hours * 3600 + minutes * 60 + seconds
+
+
+def convert_to_time_string(total_seconds):
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) // 60)
+    seconds = int(total_seconds % 60)
+    if hours == 0:
+        return f"{minutes} min, {seconds} sec"
+    elif minutes == 0:
+        return f"{hours} hours, {seconds} sec"
+    elif seconds == 0:
+        return f"{hours} hours, {minutes} min"
+    else:
+        return f"{hours} hours, {minutes} min, {seconds} sec"
+
+
+def avghr(df):
+    beats = df.totalBeats.sum()
+    dur = df.mDuration.sum()
+    bpm = round(beats / dur, 2)
+    return bpm
+
+df["sDuration"] = df["Duration"].apply(convert_to_seconds)
+df["mDuration"] = df["sDuration"] / 60
+df["hDuration"] = df["mDuration"] / 60
+
+df["totalBeats"] = round(
+    df["mDuration"] * df["bpm-Avg."],
+)
+
+run = df[df["Type"] == "Running"]
+bike = df[df["Type"] == "Cycling"]
+walk = df[df["Type"] == "Walking"]
+golf = df[df["Type"] == "Golf"]
+
+for g in [run, bike, walk, golf]:
+    g["cumkm"] = g["km"].cumsum()
+    g["cumdur"] = g['hDuration'].cumsum()
+
+# Streamlit main page
+st.title("Workout Dashboard")
+
+st.write(f"Total Activies: {len(df)}")
+st.write(f"Total Duration: {convert_to_time_string(df.sDuration.sum())}")
+st.write(f"Average Duration: {convert_to_time_string(df.sDuration.sum()/len(df))}")
+st.write(f"Total Distance: {df.km.sum()} km")
+st.write(f"Average Distance: {round(df.km.mean(),2)} km")
+st.write(f"Total Heart Beats: {df.totalBeats.sum():,.0f}")
+st.write(f"Average BPM: {avghr(df)}")
+
+# Activity pages
+def display_summary_and_raw_data(data, title):
+    st.subheader(title)
+    tab1, tab2, tab3, tab4 = st.tabs(["Summary", "Distributions", "Charts", "Raw Data"])
+    with tab1:
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write(f"Total {title}: {len(data)}")
+            st.write(f"Total Duration: {convert_to_time_string(data.sDuration.sum())}")
+            st.write(f"Total Distance: {data.km.sum()} km")
+            st.write(f"Total Heart Beats: {data.totalBeats.sum():,.0f}")    
+
+        with col2:
+            st.write(
+                f"Average Duration: {convert_to_time_string(data.sDuration.sum()/len(data))}"
+            )
+            st.write(f"Average Distance: {round(data.km.mean(),2)} km")
+            st.write(f"Average BPM: {avghr(data)}")
+        with col3:
+            st.write(f"Median Duration {convert_to_time_string(data.sDuration.median())}")
+            st.write(f"Median Distance: {round(data.km.median(),2)} km")
+            st.write(f"Median BPM: {data['bpm-Avg.'].median()}")
+    with tab2:
+        with st.expander("Boxplot"):
+            columns = ['km', 'mDuration', 'bpm-Avg.', 'bpm-hi', 'km/h', 'Cals/h']
+            fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(15, 15))
+            axes = axes.flatten()
+            for i, column in enumerate(columns):
+                sns.boxplot(x=data[column], ax=axes[i])
+                axes[i].set_title(column)
+            plt.suptitle('Boxplots')
+            plt.tight_layout()
+            st.pyplot(fig)
+        
+        def hist(col):
+            fig, ax = plt.subplots()
+            ax.hist(data[col], bins=10, edgecolor='black')
+            ax.set_title(f"Histogram of {col}")
+            ax.set_xlabel(col)
+            ax.set_ylabel("Frequency")
+            ax.yaxis.set_major_locator(MultipleLocator(1))
+            plt.tight_layout()
+            st.pyplot(fig)
+
+        with st.expander("Avg BPM"):
+            hist('bpm-Avg.')
+
+        with st.expander("Max BPM"):
+            hist('bpm-hi')
+
+        with st.expander("Distance"):
+            hist('km')
+        
+        with st.expander("Duration (min)"):
+            hist('mDuration')
+
+        with st.expander("Speed (km/h)"):
+            hist('km/h')
+
+    with tab3:
+        with st.expander('Cumulative Distance'):
+            fig, ax = plt.subplots()
+            ax.plot(data['ISO'], data['cumkm'])
+            plt.title('Cumulative Distance')
+            plt.ylabel('Distance (km)')
+            plt.xticks(rotation=45)
+            st.pyplot(fig)
+
+        with st.expander('Cumulative Duration'):
+            fig, ax = plt.subplots()
+            ax.plot(data['ISO'], data['cumdur'])
+            plt.title('Cumulative Duration')
+            plt.ylabel('Time (h)')
+            plt.xticks(rotation=45)
+            st.pyplot(fig)
+
+    with tab4:
+        st.dataframe(data)
+
+# Usage:
+tabs = ["Run", "Bike", "Walk", "Golf"]  # Add more tabs as needed
+data_dict = {
+    "Run": run,
+    "Bike": bike,
+    "Walk": walk,
+    "Golf": golf,
+}  
+
+tab_run, tab_bike, tab_walk, tab_golf = st.tabs(tabs)  # Create main tabs
+
+with tab_run:
+    display_summary_and_raw_data(data_dict["Run"], "Runs")
+
+with tab_bike:
+    display_summary_and_raw_data(data_dict["Bike"], "Rides")
+
+with tab_walk:
+    display_summary_and_raw_data(data_dict["Walk"], "Walks")
+
+with tab_golf:
+    display_summary_and_raw_data(data_dict["Golf"], "Golf")
+
+st.markdown('\n')
+st.markdown('---')
+st.markdown("Made with ❤️ by [palmerac](https://github.com/palmerac)")
